305d3d01a9008b9abeb9c6db25428af3f792eb00
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:59:51 -0700

    base: Spoof build fingerprint for Google Play Services [1/2]
    SafetyNet's CTS profile attestation checks whether Build.FINGERPRINT
    matches that of the device's stock OS, which has passed CTS testing.
    Spoof the fingerprint for Google Play Services to help pass SafetyNet.
    
    NB: This code is under the gmscompat package, but it does not depend on
    any code from gmscompat.
    
    arrow edits:
     - Correct prop name for our logic. We are planning to use fp for different devices (like old oreo or follow new Pixel's).
    
    Signed-off-by: palaych <palaych@arrowos.net>
    Change-Id: I26a2498eb2e2163933303b03f6d516e5fb30fe51

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -57,6 +57,7 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
+import com.android.internal.gmscompat.AttestationHooks;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1162,7 +1163,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        maybeSpoofBuild(app);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
     
@@ -1180,7 +1181,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        maybeSpoofBuild(app);
+        AttestationHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
 
